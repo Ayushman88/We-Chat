b46b5c33de431a8f1c88307dd655f8618802bef2
Ayushman Mishra <132093864+Ayushman88@users.noreply.github.com>
2024-03-11 01:22:14 +0530

    Added Speed Insights

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { ChatEngine } from "react-chat-engine";
 
 import ChatFeed from "./components/ChatFeed";
 import LoginForm from "./components/LoginForm";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 import "./App.css";
 
 const projectID = "8d9a726b-47fd-4b46-b4d0-e0ecde91534e";
@@ -10,18 +11,21 @@ const App = () => {
   if (!localStorage.getItem("username")) return <LoginForm />;
 
   return (
-    <ChatEngine
-      height="100vh"
-      projectID={projectID}
-      userName={localStorage.getItem("username")}
-      userSecret={localStorage.getItem("password")}
-      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
-      onNewMessage={() =>
-        new Audio(
-          "https://chat-engine-assets.s3.amazonaws.com/click.mp3"
-        ).play()
-      }
-    />
+    <>
+      <ChatEngine
+        height="100vh"
+        projectID={projectID}
+        userName={localStorage.getItem("username")}
+        userSecret={localStorage.getItem("password")}
+        renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+        onNewMessage={() =>
+          new Audio(
+            "https://chat-engine-assets.s3.amazonaws.com/click.mp3"
+          ).play()
+        }
+      />
+      <SpeedInsights />
+    </>
   );
 };
 
